272255f00f6436af955ca0e8169b829ef9a1f958
Israel Chima <israelchima91@gmail.com>
2025-10-06 09:25:35 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, 'useState } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Mail, Lock, Eye, EyeOff } from 'lucide-react';
