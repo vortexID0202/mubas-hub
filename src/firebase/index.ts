4d073d0c3d818ad64848c2a370900e7f41338470
Israel Chima <israelchima91@gmail.com>
2025-10-07 21:06:46 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,7 +1,15 @@
 import {initializeApp, getApps, type FirebaseApp} from 'firebase/app';
 import {getAuth, type Auth} from 'firebase/auth';
 import {getFirestore, type Firestore} from 'firebase/firestore';
-import {firebaseConfig} from './config';
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREASE_APP_ID,
+};
 
 export function initializeFirebase(): {
   app: FirebaseApp;
