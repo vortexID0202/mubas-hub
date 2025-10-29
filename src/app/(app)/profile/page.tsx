49022a70f6d2cc34f549d74373f7883c6574719b
Israel Chima <israelchima91@gmail.com>
2025-10-29 11:11:18 +0000

    ive observed another issue the user email  in the profile in desktop vie

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -328,7 +328,7 @@ export default function ProfilePage() {
                         </Button>
                     </div>
                   <h1 className="mt-4 text-center font-headline text-2xl font-bold">{userProfile.fullName}</h1>
-                  <p className="mt-1 text-center text-muted-foreground">
+                  <p className="mt-1 text-center text-muted-foreground break-all">
                     {userProfile.email}
                   </p>
                   <div className="mt-4 w-full text-center">
@@ -575,5 +575,3 @@ export default function ProfilePage() {
     </>
   );
 }
-
-    
